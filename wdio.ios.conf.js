ff28a911426324ca8c14210620f0ac61313c799f
lucas <lucas@MacBook-Pro-de-lucas.local>
2024-08-03 12:39:05 -0300

    adding terminateApp and launchApp

--- wdio.ios.conf.js
+++ wdio.ios.conf.js
@@ -309,11 +309,22 @@ export const config = {
    */
   // afterAssertion: function(params) {
   // }
-  fterTest: async function (
+  afterTest: async function (
     test,
     context,
     { error, result, duration, passed, retries }
   ) {
     await driver.takeScreenshot();
+    await driver.execute("mobile: terminateApp", {
+      bundleId: "br.com.lojaebac",
+    });
+  },
+  beforeTest: async function () {
+    let state = await driver.queryAppState("br.com.lojaebac");
+    if (state !== 4) {
+      await driver.execute("mobile: launchApp", {
+        bundleId: "br.com.lojaebac",
+      });
+    }
   },
 };
