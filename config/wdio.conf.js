79b9460e350b3a47812ce89d4ed7b70949484014
Lucas Evangelista <lucas1994@gmail.com>
2024-08-04 22:50:47 -0300

    removing logs and adding env

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -4,14 +4,6 @@ import { sauceConf } from "./sauce.conf.js";
 import "dotenv/config";
 
 function getConfig() {
-  console.log(
-    process.env.ENVIRONMENT,
-    process.env.PLATFORM,
-    process.env.REPORT,
-    process.env.SAUCE_USERNAME,
-    process.env.SAUCE_ACCESS_KEY
-  );
-
   switch (process.env.ENVIRONMENT) {
     case "local":
       return localConf;
