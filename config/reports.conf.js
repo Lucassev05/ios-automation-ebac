71c992610bf6227c85a5540ebc595ea6cd1d744a
Lucas Evangelista <lucas1994@gmail.com>
2024-08-04 23:33:16 -0300

    updating report env on reports.config.js

--- config/reports.conf.js
+++ config/reports.conf.js
@@ -1,9 +1,16 @@
-export let reportersConf = process.env.REPORTER == 'true' ? {
-    reporters: ['spec',
-        ['allure', {
-            outputDir: 'allure-results',
-            disableWebdriverStepsReporting: true,
-            disableWebdriverScreenshotsReporting: false
-        }]
-    ],
-} : {}
+export let reportersConf =
+  process.env.REPORT == "true"
+    ? {
+        reporters: [
+          "spec",
+          [
+            "allure",
+            {
+              outputDir: "allure-results",
+              disableWebdriverStepsReporting: true,
+              disableWebdriverScreenshotsReporting: false,
+            },
+          ],
+        ],
+      }
+    : {};
